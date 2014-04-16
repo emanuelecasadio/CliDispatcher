f672e49d8ce1bf159a10dd9b6cd785a82dc0fd28
Emanuele Casadio <emanuele.casadio@gmail.com>
2014-04-16 13:05:50 +0100

    Capability to retreive private ips of workers added (2)

--- src/main/java/com/kopjra/beanstalkd/cloudwatch/AmazonInformer.java
+++ src/main/java/com/kopjra/beanstalkd/cloudwatch/AmazonInformer.java
@@ -2,6 +2,7 @@ package com.kopjra.beanstalkd.cloudwatch;
 import java.io.*;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 
@@ -10,25 +11,31 @@ import com.amazonaws.services.cloudwatch.*;
 import com.amazonaws.services.cloudwatch.model.*;
 import com.amazonaws.AmazonClientException;
 import com.amazonaws.AmazonServiceException;
+import com.amazonaws.services.ec2.*;
+import com.amazonaws.services.ec2.model.*;
 
 /**
- * Simplest CloudWatch metric updater
- * 
+ * CloudWatch metric updater and EC2 instances monitoring tool
+ *
+ * It updates a custom CloudWatch metric based on the length of the
+ * queues and retrieves the list of worker IPs available at the moment
+ *
  * @author Emanuele Casadio
  *
  */
-public class CloudwatchInformer {
-  
+public class AmazonInformer {
+
   public static void main( String[] args ) throws IOException {
 	  final String env = "staging";
-    AWSCredentials credentials = 
-      new PropertiesCredentials( 
-          CloudwatchInformer.class.getResourceAsStream( "AwsCredentials.properties" )
+    AWSCredentials credentials =
+      new PropertiesCredentials(
+          AmazonInformer.class.getResourceAsStream( "AwsCredentials.properties" )
     );
-    AmazonCloudWatch cw = new AmazonCloudWatchClient(credentials);
-    cw.setEndpoint("monitoring.eu-west-1.amazonaws.com");
     
       try {
+    	  // Publish Queue Length
+    	  AmazonCloudWatch cw = new AmazonCloudWatchClient(credentials);
+    	  cw.setEndpoint("monitoring.eu-west-1.amazonaws.com");
     	  com.surftools.BeanstalkClient.Client bc = new com.surftools.BeanstalkClientImpl.ClientImpl("127.0.0.1", 11300, true);
     	  List<String> lt = bc.listTubes();
     	  double tot = 0.0;
@@ -50,7 +57,28 @@ public class CloudwatchInformer {
     	  data.setNamespace("queues-"+env);
     	  data.setMetricData(cmd);
           cw.putMetricData(data);
-          
+
+          // Get workers' private ips
+          AmazonEC2 ec2 = new AmazonEC2Client(credentials);
+          ec2.setEndpoint("ec2.eu-west-1.amazonaws.com");
+          DescribeInstancesRequest req = new DescribeInstancesRequest();
+          Collection<Filter> filters = new HashSet<Filter>();
+          List<String> values = new ArrayList<String>();
+          values.add("worker");
+          filters.add(new Filter("tag-key",values));
+          req.setFilters(filters);
+          DescribeInstancesResult ec2res = ec2.describeInstances(req);
+          List<Reservation> reservations = ec2res.getReservations();
+          for (Reservation reservation : reservations) {
+			List<Instance> instances = reservation.getInstances();
+			for(Instance instance : instances){
+				InstanceState s = instance.getState();
+				if(s.getName().equals("running")){
+					System.out.println(instance.getPrivateIpAddress());
+				}
+			}
+		}
+                    
       } catch (AmazonServiceException ase) {
           System.err.println( "AmazonServiceException" );
       } catch (AmazonClientException ace) {
