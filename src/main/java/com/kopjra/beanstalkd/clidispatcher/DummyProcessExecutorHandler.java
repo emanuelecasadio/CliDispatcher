002a84ab795a18b746a1bfbe83e448f75cffeeca
Emanuele Casadio <emanuele.casadio@gmail.com>
2014-05-23 22:05:52 +0200

    From slf4j-simple to slf4j-log4j12 for centralized logging

--- src/main/java/com/kopjra/beanstalkd/clidispatcher/DummyProcessExecutorHandler.java
+++ src/main/java/com/kopjra/beanstalkd/clidispatcher/DummyProcessExecutorHandler.java
@@ -1,15 +1,23 @@
 package com.kopjra.beanstalkd.clidispatcher;
 
 import com.dzone.java.ProcessExecutorHandler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class DummyProcessExecutorHandler implements ProcessExecutorHandler {
 
+	private Logger logger;
+	
+	public DummyProcessExecutorHandler(){
+		logger = LoggerFactory.getLogger(DaemonWrapper.class);
+	}
+	
 	public void onStandardOutput(String msg) {
-		System.out.println(msg);
+		logger.info(msg);
 	}
 
 	public void onStandardError(String msg) {
-		System.err.println(msg);
+		logger.error(msg);
 	}
 
 }
